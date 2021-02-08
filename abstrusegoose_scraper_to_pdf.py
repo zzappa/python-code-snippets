dfd09c4896ddb3da71ce1a1af41e5b3a71af4e26
zzappa <agolenchenko@gmail.com>
2021-02-08 10:16:29 +0300

    Fix comic scraper

--- abstrusegoose_scraper_to_pdf.py
+++ abstrusegoose_scraper_to_pdf.py
@@ -5,7 +5,7 @@ import logging
 import re
 from io import BytesIO
 
-import lxml
+from lxml import html as lxml_html
 import requests
 from PIL import Image
 from bs4 import BeautifulSoup
@@ -19,7 +19,7 @@ def add_image(img, text, size):
     pdf.image(img)
     pdf.ln(5)
     text = text.encode('latin-1', 'ignore').decode('latin-1')
-    pdf.multi_cell(ln=1, w=0, h=20, align='L', txt=text)
+    pdf.multi_cell(ln=1, w=0, h=26, align='L', txt=text)
 
 
 def fetch_image(link):
@@ -46,7 +46,9 @@ def get_comic(link):
         soup = BeautifulSoup(r.text, 'html5lib')
         txt = soup.find("div", {"id": "blog_text"})
         txt = html.unescape(str(txt).lstrip('<div id="blog_text"><p>\n<p>').rstrip('</p></div></p>\n'))
-        txt = str(lxml.html.fromstring(txt).text_content())
+        txt = str(lxml_html.fromstring(txt).text_content())
+        for item in ('r/>', 'align="center">', 'a href="', '" target="_blank"', 're>', 'm>', 'strong>', '/p>'):
+            txt = txt.replace(item, ' ')
         txt = txt.replace('r/>', '') + "\n" + src
     except Exception:
         txt = src
@@ -59,13 +61,13 @@ def get_comic(link):
     return i, text
 
 
-pdf = FPDF(unit='pt')
-pdf.set_font("Helvetica", size=14)
+pdf = FPDF(unit = 'pt')
+pdf.set_font("Times", size=20)
 img1 = fetch_image('https://abstrusegoose.com/strips/conv_sub_panel_small.png')
-add_image(img1, 'Convergent Subsequence' + '\n' + link + '1', size=(img1.width + 50, img1.height + 400))
-for i in range(2, 612):
+add_image(img1, 'Convergent Subsequence' + '\n' + link + '1', size=(img1.width + 50, img1.height + 200))
+for i in range(2, 615):
     url = link + str(i)
     img, txt = get_comic(url)
     if img:
-        add_image(img, txt, size=(img.width + 50, img.height + 500))
+        add_image(img, txt, size=(img.width + 50, img.height + 550))
 pdf.output("abstrusegoose.pdf")
